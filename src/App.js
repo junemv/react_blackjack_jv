2438fe7ca8b1bbc67f0b2095d61c53a17c588ccd
junemv <vintydesign@gmail.com>
2023-12-18 15:21:26 -0800

    built out runGame function and initializeHands function. Considering how to handle global variables

--- src/App.js
+++ src/App.js
@@ -5,41 +5,88 @@ import { getRandomInt } from './lib/func';
 
 function App() {
 
-  // on start button click 
-  const startGame = (cardScore, cardBank, scores, dealerHand, user1Hand, getRandomInt) => {
-    // starts dealer and player w/ 1 card
+  // Run on start or reset button click 
+  const runGame = (cardScore, cardBank, scores, dealerHand, user1Hand, getRandomInt) => {
+    // starts dealer and player w/ 2 cards
       // dealer's is face down - will add when graphics are added (card dict should have a value of [quantity, card_face])
       // second card is drawn face up for player and dealer
     console.log('HELLO I WORK')
     console.log('card bank', cardBank)
     console.log('dealer hand', dealerHand)
     console.log('user hand', user1Hand)
+
+    // the deck of cards - mutated as cards are drawn
+    var cardBankClone = structuredClone(cardBank);
+
+    // player hands
+    var dealerHandClone = structuredClone(dealerHand);
+    var user1HandClone = structuredClone(user1Hand);
+
+    // player scores for current game
+    var scoresClone = structuredClone(scores)
+
+    var initialHandObj = initializeHands(cardBankClone, dealerHandClone, user1HandClone, scoresClone, cardScore)
+    // update w/ initial hand results
+    cardBankClone = initialHandObj.cardBank;
+    dealerHandClone = initialHandObj.dealerHand;
+    user1HandClone = initialHandObj.user1Hand;
+    scoresClone = initialHandObj.scores;
     
-    //TODO - this isn't going to work, research pop, I want to pop a random index from the card bank using getRandomInt
-    // user1Hand.push(cardBank.pop(getRandomInt(cardBank.length())));
+    
+  }
+
+  // Run to build initial hand
+  const initializeHands = (cardBank, dealerHand, user1Hand, scores, cardScore) => {
+
     while (dealerHand.length < 2) {
       console.log("I'm in the loop")
       let i = getRandomInt(cardBank.length);
-      // use random to generate random number for i
-      // push element from i to user hands, then delete cardBank[i]
+
+      // add card to dealer's hand
       user1Hand.push(cardBank[i])
+      console.log(cardBank[i][0], "score:", cardScore[cardBank[i][0]])
+      
+      // update dealer's score with card point value
+      scores["user1"] = scores["user1"] + cardScore[cardBank[i][0]] 
+      console.log('user1 score', scores["user1"])
+      
       cardBank.splice(i, 1)
 
       console.log('i',i)
 
       i = getRandomInt(cardBank.length)
+      // add card to dealer's hand
       dealerHand.push(cardBank[i])
+      
+      // update dealer's score with card point value
+      scores["dealer"] = scores["dealer"] + cardScore[cardBank[i][0]] 
+      console.log('dealer score', scores["dealer"])
+      
       cardBank.splice(i, 1)
+      
       console.log('i',i)
       console.log('cardBank length', cardBank.length)
       console.log('cardBank after splice', cardBank)
       console.log('userHand:', user1Hand, 'dealerHand', dealerHand)
+
+      return { "cardBank": cardBank, "dealerHand": dealerHand, "user1Hand": user1Hand, "scores": scores }
+
     }
+    
+  }
+
+  const updateScore = (val, player, scores) => {
+    var scoresClone = structuredClone(scores)
+
+    scoresClone.player += val;
+    console.log(scoresClone)
+    return scoresClone
   }
   
   // on hit button click
   const drawCard = () => {
     // draw 1 card to the user's hand from the bank
+
     // needs condition for dealer to draw a card if hand point value is < 16
     // needs a condition for bust?
   }
@@ -53,7 +100,8 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={()=>startGame(cardScore, cardBank, scores, dealerHand, user1Hand, getRandomInt)}>PUSH ME</button>
+      <button onClick={()=>runGame(cardScore, cardBank, scores, dealerHand, user1Hand, getRandomInt)}>START GAME</button>
+      <button onClick={()=>drawCard()}>HIT</button>
     </div>
   );
 }
